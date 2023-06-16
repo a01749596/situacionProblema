46d059b4e790049aa4864a19174606cda389e512
a01749596 <a01749596@tec.mx>
2023-06-15 21:38:22 -0600

    Creación del menu

---
+++ Menu.cpp
@@ -0,0 +1,28 @@
+#include "Menu.h"
+
+void Menu::mostrarMenu()
+{
+    while(true)
+    {
+        cout << " Servicio de streaming: " << endl; 
+        cout << "1) Cargar archivo" << endl; 
+        cout << "2) Mostrar videos (calificacion / genero)" << endl; 
+        cout << "3) Mostrar episodios de una serie" << endl; 
+        cout << "4) Mostrar peliculas por su calificacion" << endl; 
+        cout << "5) Califica un video" << endl; 
+        cout << "6) Promedio de una serie" << endl; 
+        cout << "7) Salir" << endl; 
+
+        int opcion; 
+        cout << "Selecciona una opcion" << endl;
+        cin >> opcion; 
+
+        if(opcion == 1)
+        {
+
+        } else if(opcion == 2)
+        {
+            
+        }
+    }
+}
---
+++ Menu.h
@@ -0,0 +1,13 @@
+#ifndef MENU_H
+#define MENU_H
+#include <string> 
+#include <iostream>
+using namespace std; 
+
+class Menu 
+{
+public: 
+    void mostrarMenu(); 
+};
+
+#endif
